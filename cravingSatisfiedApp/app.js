3d2215291a4363336987a8f2634271a5a22758e0
Edri <117730224+EdrianaJoy@users.noreply.github.com>
2024-07-08 20:38:54 +1000

    Create result page

--- cravingSatisfiedApp/app.js
+++ cravingSatisfiedApp/app.js
@@ -5,13 +5,15 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var selectRouter = require('./routes/select');
+var resultsRouter = require('./routes/results');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -26,22 +28,7 @@ app.use('/jquery', express.static(path.join(__dirname, '/node_modules/jquery')))
 
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+app.use('/select', selectRouter);
+app.use('/results', resultsRouter);
 
 module.exports = app;
